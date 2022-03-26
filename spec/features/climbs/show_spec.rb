5a5d8fb2b1b8c25ed0ca0ed805a27594f99a035e
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-26 15:22:54 -0600

    add climbs show

--- spec/features/climbs/show_spec.rb
+++ spec/features/climbs/show_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'the climbs show page' do
     fun_route = smith_rock.climbs.create!(name: 'Test Fun Route', pitches: 1, trad: false)
     long_route = smith_rock.climbs.create!(name: 'Test Long Route', pitches: 7, trad: true)
     visit "/climbs"
-    save_and_open_page
 
     expect(page).to have_content(fun_route.name)
     expect(page).to have_content(long_route.name)
@@ -14,5 +13,18 @@ RSpec.describe 'the climbs show page' do
     expect(page).to have_content(long_route.pitches)
     expect(page).to have_content(fun_route.trad)
     expect(page).to have_content(long_route.trad)
+  end
+
+  it 'shows climb with given id' do
+    smith_rock = Crag.create!(name: 'Test Smith Rock', year_round: true, outhouses: 2)
+    fun_route = smith_rock.climbs.create!(name: 'Test Fun Route', pitches: 1, trad: false)
+    long_route = smith_rock.climbs.create!(name: 'Test Long Route', pitches: 7, trad: true)
+    visit "/climbs/#{fun_route.id}"
+    save_and_open_page
+
+    expect(page).to have_content(fun_route.name)
+    expect(page).to have_content(fun_route.pitches)
+    expect(page).to have_content(fun_route.trad)
+    expect(page).to_not have_content(long_route.name)
   end
 end
